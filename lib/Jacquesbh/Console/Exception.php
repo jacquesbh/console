710c75f3213603d9374720bac6928be9a1d41646
Jacques Bodin-Hullin <jacques@bodin-hullin.net>
2012-06-09 18:52:03 +0200

    moved to lib and added Http Authentication ;)

---
+++ lib/Jacquesbh/Console/Exception.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @license GNU Lesser General Public License
+ * @author Jacques Bodin-Hullin <jacques@bodin-hullin.net>
+ * @package Jacquesbh/Console
+ * @version 1.1
+ *
+ * @github http://github.com/jacquesbh/console
+ */
+
+/**
+ * Namespace
+ */
+namespace Jacquesbh\Console;
+
+/**
+ * Console Exception
+ *
+ * @class Exception
+ * @namespace Jacquesbh\Console
+ */
+class Exception extends \Exception
+{}
+
